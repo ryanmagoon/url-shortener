6fdd310f0e81d303ae88019bab65e2e29995325c
Ryan Magoon <mr.magoon5@gmail.com>
2016-08-24 16:11:51 -0400

    trying to fix host for db

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ let config = {};
 config.db = {};
 // the URL shortening host - shortened URLs will be this + base58 ID
 // i.e: http://localhost:3000/3Ys
-config.webhost = 'http://localhost:3000/';
+config.webhost = 'https://peaceful-caverns-62358.herokuapp.com/';
 
 // your MongoDB host and database name
 config.db.host = 'localhost';
